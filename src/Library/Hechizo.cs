3dbcabfa3b0ba5f7b5fa3de97b5ae0385c68472c
Marcelo Arrarte <32116762+MarceloArrarte@users.noreply.github.com>
2021-09-14 16:49:16 -0300

    Clase Hechizo y trabajo en clase ConsoleLogger

---
+++ src/Library/Hechizo.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+
+namespace Program {
+    public class Hechizo {
+        private static string[] _tiposHechizos = {"Daño", "Curación"};
+        public string Nombre { get; set; }
+        public string _tipoEfecto;
+        public string TipoEfecto {
+            get {
+                return _tipoEfecto;
+            }
+            set {
+                if (_tiposHechizos.Contains(value)) {
+                    this._tipoEfecto = value;
+                }
+            }
+        }
+        public int Poder { get; set; }
+
+        public Hechizo(string nombre, string tipoEfecto, int poder) {
+
+        }
+    }
+}
