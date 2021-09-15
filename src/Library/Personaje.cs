dc0698cd4b2cd623baf262f96f712add793bde7f
Dana Castroman <danacastroman@gmail.com>
2021-09-15 18:53:18 -0300

    Clase Personaje

--- src/Library/Personaje.cs
+++ src/Library/Personaje.cs
@@ -3,59 +3,80 @@ using System.Collections;
 
 namespace Program
 {
-  public class Personaje{
-    public Personaje(string nombre, string especie){
+  public class Personaje
+  {
+    const int ataqueBase = 5;
+    const int defensaBase = 2;
+    public Personaje(string nombre, string especie)
+    {
       this.Nombre = nombre;
       this.Especie = especie;
       this.HP = 100;
     }
 
-    private ArrayList inventario;
     private int hp;
-
-    private int ataque;
-    public int Ataque{
-      get{
-        return this.ataque;
-      }
-      set {
-        this.ataque = 5 + VerAtaque(); 
-      }
-    }
-
-    private int defensa;
-    public int Defensa{
-      get{
-        return this.defensa;
-      }
-      set{
-        this.defensa = 5 + VerDefensa();
-      }
-    }
 
     public string Nombre{get; set;}
     public string Especie{get; set;}
-    public ArrayList[] Inventario{get; set;}
+    private ArrayList inventario;
+    public void AddItem(Item item)
+    {
+      inventario.Add(item);
+    }
+    public void RemoveItem(Item item)
+    {
+      inventario.Remove(item);
+    }
+
+    public Item Arma 
+    {
+      get
+      {
+        foreach (Item item in inventario)
+        {
+          if(item.Tipo == "ARMA")
+          {
+            return item;
+          }
+        }
+        return null;
+      }
+    }
+
+
     public int HP{get; set;}
 
-    public int VerAtaque(){
-      int arma = 0;
-      foreach (Item item in inventario){
-        if(item.Tipo == "ARMA"){
-          arma = item.DMG;
+    public int Ataque
+    {
+      get {
+        int arma = 0;
+        foreach (Item item in inventario)
+        {
+          if(item.Tipo == "ARMA")
+          {
+            if(arma < item.DMG){
+              arma = item.DMG;
+            }
+          }
         }
+        return arma + ataqueBase;
       }
-      return arma;
+      
     }
 
-    public int VerDefensa(){
-      int def = 0;
-      foreach (Item item in inventario){
-        if(item.Tipo == "ARMADURA"){
-          def = item.DEF;
+    public int Defensa
+    {
+      get{
+        int def = 0;
+        foreach (Item item in inventario)
+        {
+          if(item.Tipo == "ARMADURA")
+          {
+            def = item.DEF;
+          }
         }
+        return def + defensaBase;
       }
-      return def;
     }
   }
-}
+} 
