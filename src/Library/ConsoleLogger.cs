9ef838582ea31f253194f6a6bd76f3ca3a98d953
SANTIAGO ALONSO <SANTIAGO.RALONSO@correo.ucu.edu.uy>
2021-09-15 23:47:38 -0300

    update manual de logger

--- src/Library/ConsoleLogger.cs
+++ src/Library/ConsoleLogger.cs
@@ -6,7 +6,37 @@ namespace Program {
         public static void ImprimirAtaque(Personaje atacante, Personaje atacado) {
             StringBuilder sb = new StringBuilder();
             sb.Append($"{atacante.Nombre} atacó a {atacado.Nombre} con {atacante.Arma}.");
-            sb.Append($" {atacado.Nombre} quedó con {atacado.Hp} HP.");
+            if (atacado.HP > 0) {
+                sb.Append($" {atacado.Nombre} quedó con {atacado.HP} HP.");
+            }
+            else {
+                sb.Append($" {atacado.Nombre} ha muerto.");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        public static void ImprimirCuracion(Personaje curador, Personaje curado) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{curador.Nombre} curó a {curado.Nombre}.");
+            sb.Append($" {curado.Nombre} quedó con {curado.HP} HP.");
+            Console.WriteLine(sb.ToString());
+        }
+
+        public static void ImprimirLanzamientoHechizoDaño(Personaje lanzador, Hechizo hechizo, Personaje objetivo) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{lanzador.Nombre} lanzó el hechizo {hechizo.Nombre} a {objetivo.Nombre}, ");
+            switch (hechizo.TipoEfecto) {
+                case "Daño":
+                    sb.Append($"causándole {hechizo.Poder} puntos de daño.");
+                    break;
+                case "Curación":
+                    sb.Append($"curándole {hechizo.Poder} puntos de vida.");
+                    break;
+            }
+            sb.Append($" {objetivo.Nombre} quedó con {objetivo.HP} HP.");
+            if (objetivo.HP == 0) {
+                sb.Append($" {objetivo.Nombre} ha muerto.");
+            }
             Console.WriteLine(sb.ToString());
         }
     }
